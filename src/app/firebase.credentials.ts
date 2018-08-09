b764c1e198d3d5575f01e13c2e7e3f086771e142
Indira <indiraghandy22@gmail.com>
2018-08-09 10:22:36 -0500

    Crud on firebase

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA_VlRDUbiyA5BfzL5UwaspHSuhExdoa2Y",
+  authDomain: "ionicfirebasecrud-211515.firebaseapp.com",
+  databaseURL: "https://ionicfirebasecrud-211515.firebaseio.com",
+  projectId: "ionicfirebasecrud-211515",
+  storageBucket: "ionicfirebasecrud-211515.appspot.com",
+  messagingSenderId: "1080270039705"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  site = {
+    url: 'javasampleapproach.com',
+    description: 'Java Technology - Spring Framework'
+  };
 
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
+    this.db.list('site').push(this.site);
   }
 
 }
